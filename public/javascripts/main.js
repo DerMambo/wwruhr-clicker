532807fe695e3f66f746e62a2b8bce40781ba3b0
Manuel Schoebel <mail@manuelschoebel.de>
2013-02-28 19:06:23 +0100

    ip set

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -8,4 +8,4 @@ $(document).ready(function(){
 });
 
 var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
-var socket = io.connect('http://localhost:3000/');
+var socket = io.connect('http://10.55.1.105:3000/');
